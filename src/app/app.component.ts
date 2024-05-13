a7b9f9e6877dde93aac7c828f2d53303ce5cd351
Michael David Machacuay <mdavidmachacuay@icloud.com>
2024-05-13 18:46:03 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, collectionData, doc, docData } from '@angular/fire/firestore';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'cotizador';
+  item$!: Observable<any>;
+  firestore: Firestore = inject(Firestore);
+
+  solesAmount: number = 0;
+  dollarAmount: number = 0;
+  salePrice: number = 0;
+  purchasePrice: number = 0;
+
+  public flexDirection: string = 'column';
+
+  toggleFlexDirection() {
+    this.flexDirection = (this.flexDirection === 'column') ? 'column-reverse' : 'column';
+  }
+
+  ngOnInit() {
+    const itemDoc = doc(this.firestore, 'rates/TDmXIypgLKKfNggHHSnw');
+    this.item$ = docData(itemDoc);
+
+    this.item$.subscribe((data) => {
+      console.log(data);
+      this.salePrice = data.sale_price;
+      this.purchasePrice = data.purchase_price;
+    });
+  }
+
+  changeSolesAmount() {
+    this.solesAmount = this.dollarAmount * this.purchasePrice;
+  }
+
+  changeDollarAmount() {
+    this.dollarAmount = this.solesAmount / this.salePrice;
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {  
+    apiKey: "AIzaSyAs4-CG_yVDa4keSwGONSRb1kpA6usPEww",  
+    authDomain: "exchangeratecs-6eea8.firebaseapp.com",  
+    projectId: "exchangeratecs-6eea8", 
+    storageBucket: "exchangeratecs-6eea8.appspot.com",  
+    messagingSenderId: "1028871012583",  
+    appId: "1:1028871012583:web:64dd5566da88be0cc16242"
+  }
+};
